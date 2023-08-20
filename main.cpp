a139b808fab05af5220acb22902fbe661e0c6e49
MarcChedefaux <chedefaux.marc@gmail.com>
2023-08-20 10:31:06 +0200

    Addition of important submodules

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <opencv4/opencv2/opencv.hpp>
+#include <tqdm.hpp>
 #include <iostream>
+#include <unistd.h>
 
 int main() {
     std::cout << "Hello World" << std::endl;
     
 
     int image_width = 500, image_height = 500;
-    short *image = (short*)malloc(image_width*image_height*3*sizeof(short));
+    
     cv::Mat img(image_width, image_height,CV_8UC3);
 
-    for (int i = 0; i < image_width; i++) {
+    for (int i : tq::trange(image_width)) {
         for (int j = 0; j<image_height; j++) {
             
             auto r = double(i) / (image_width-1);
